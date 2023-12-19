9100b3bddc2feefb012394679ef97de034e7333b
Mariadoc1 <153285337+Mariadoc1@users.noreply.github.com>
2023-12-19 14:21:28 +0000

    Anded the basic form handling to the javascript. Added typewriter function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Recipe will appear here",
+    autoStart: true,
+    delay: 10,
+    cursor: null,
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
